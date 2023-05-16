9c3159e8d59fb21611c23845db06747e3bcd1238
bradgravett <bradgravett@gmail.com>
2023-05-16 00:00:23 -0500

    more font work

--- src/main/kotlin/FrameDefParser.kt
+++ src/main/kotlin/FrameDefParser.kt
@@ -10,23 +10,27 @@ class FrameDefParser(frameFile: String) {
   val fileContents = bufferedReader.use { it.readText() }
 
   fun createImg() {
-    val size = Dimension(180, 252)
+    val size = Dimension(360, 504)
     val img = BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB)
 
     val graphics = img.createGraphics()
-    val belerenFont =
-        Font.createFont(Font.TRUETYPE_FONT, File("src/main/resources/fonts/Beleren2016-Bold.ttf"))
-
-    graphics.font = Font("Arial", Font.BOLD, 16)
-    graphics.color = Color.BLACK
 
     for (x in 0 until size.width) {
       for (y in 0 until size.height) {
-        img.setRGB(x, y, 0xff0000)
+        img.setRGB(x, y, 0x88aacc)
       }
     }
 
+    graphics.font = Font("Arial", Font.BOLD, 16)
+    graphics.color = Color.BLACK
     graphics.drawString("Test Text", 50, 50)
+
+    val belerenFont =
+        Font.createFont(Font.TRUETYPE_FONT, File("src/main/resources/fonts/Beleren2016-Bold.ttf"))
+    graphics.font = belerenFont
+      graphics.color = Color.WHITE
+
+    graphics.drawString("Test Text", 50, 100)
 
     ImageIO.write(img, "BMP", File("output/test.bmp"))
   }
